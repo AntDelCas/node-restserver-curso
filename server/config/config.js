c3c80215812c8e00502beb4d748682b1dcb121a8
Antonio <antdelcas@gmail.com>
2019-12-16 11:39:56 +0100

    restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,25 @@
 //  Puerto
 // ======
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===== 
+//  Entorno
+// ======
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+// ===== 
+//  Base de datos
+// ======
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe_user:@12345678@@cursomongo-beosp.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
